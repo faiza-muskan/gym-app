9588a82a9c81a904c5d941fb392d3a870494c007
Faiza Muskan <108839239+faiza-muskan@users.noreply.github.com>
2023-05-03 15:23:38 +0100

    added styling to menu page

--- src/components/menuPage/Menu.js
+++ src/components/menuPage/Menu.js
@@ -12,6 +12,61 @@ const Menu = () => {
   const [query, setQuery] = useState(searchParams.get("query"));
   const { sendRequest, loading } = UseHttp();
 
+  useEffect(() => {
+    setResult([
+      {
+        id: 642539,
+        title: "Falafel Burger",
+        image: "https://spoonacular.com/recipeImages/642539-312x231.png",
+      },
+      {
+        id: 651190,
+        title: "Masala-Tofu Burger",
+        image: "https://spoonacular.com/recipeImages/651190-312x231.jpg",
+      },
+      {
+        id: 637631,
+        title: "Cheesy Bacon Burger with Spicy Chipotle Aiolo Sauce",
+        image: "https://spoonacular.com/recipeImages/637631-312x231.jpg",
+      },
+      {
+        id: 638771,
+        title: "Chipotle-Salsa Burger",
+        image: "https://spoonacular.com/recipeImages/638771-312x231.jpg",
+      },
+      {
+        id: 635345,
+        title: "Blue Cheese and Mushroom Turkey Burger",
+        image: "https://spoonacular.com/recipeImages/635345-312x231.jpg",
+      },
+      {
+        id: 664011,
+        title: "Turkey Burgers",
+        image: "https://spoonacular.com/recipeImages/664011-312x231.jpg",
+      },
+      {
+        id: 648303,
+        title: "Itty Bitty Burgers",
+        image: "https://spoonacular.com/recipeImages/648303-312x231.jpg",
+      },
+      {
+        id: 642695,
+        title: "Feta Stuffed Burgers",
+        image: "https://spoonacular.com/recipeImages/642695-312x231.jpg",
+      },
+      {
+        id: 991010,
+        title: "Chicken Ranch Burgers",
+        image: "https://spoonacular.com/recipeImages/991010-312x231.jpg",
+      },
+      {
+        id: 649182,
+        title: "Lamb and Bacon Burgers",
+        image: "https://spoonacular.com/recipeImages/649182-312x231.jpg",
+      },
+    ]);
+  }, []);
+
   // const loadData = (data) => {
   //   const response = data.results;
   //   setResult(response);
@@ -47,18 +102,20 @@ const Menu = () => {
         />
         <h1 className=" font-bold text-4xl text-center text-white absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           Restaurant food and Takeaway
+          <br />
           <span className=" text-[#EB0029]">Delivered.</span>
         </h1>
         <form onSubmit={onSubmit}>
           <input
             type="search"
+            value={query}
             ref={inputRef}
             placeholder="Search"
-            className=" rounded-full py-3 px-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-24 w-[40%]"
+            className=" rounded-full py-3 px-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-24 xl:w-[40%] 2xl:w-[27%]"
           />
           <button
             type="submit"
-            className=" absolute top-1/2 left-[66%] -translate-x-1/2 -translate-y-1/2 text-white mt-24 bg-[#EB0029] px-6 py-2.5 rounded-full text-sm"
+            className=" absolute top-1/2 xl:right-[27%] 2xl:right-[35%] -translate-x-1/2 -translate-y-1/2 text-white mt-24 bg-[#EB0029] px-6 py-2.5 rounded-full text-sm"
           >
             {loading ? <Loader /> : "search"}
           </button>
--- src/components/menuPage/MenuDisplay.js
+++ src/components/menuPage/MenuDisplay.js
@@ -3,11 +3,8 @@ import React from "react";
 function MenuDisplay(props) {
   const { id, image, title, price } = props;
   return (
-    <div
-      key={id}
-      className=" container border shadow-lg rounded-lg w-[329px] h-[300px]"
-    >
-      <img src={image} alt={title} className="rounded-xl w-[329px] h-auto" />
+    <div key={id} className=" border shadow-lg rounded-xl w-[329px] h-auto">
+      <img src={image} alt={title} className="rounded-t-xl w-[329px] h-auto" />
       <div className="pl-2 py-4">
         <h1 className="text-xl font-semibold">{title}</h1>
         <p>£{price}</p>
